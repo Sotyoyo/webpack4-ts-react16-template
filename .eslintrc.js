ed56b8d9f42729b966d675fd0e1b7e3ad1418059
Sotyoyo <sotyoyo@icloud.com>
2020-01-26 21:00:45 +0800

    feat(eslint): add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+// install: eslint-plugin-import eslint-plugin-react eslint-plugin-jsx-a11y eslint-config-airbnb to active airbnb config
+module.exports = {
+    extends: 'airbnb',
+    rules: {
+        semi: ['error', 'never'], // no-use ; line end
+        indent: ['error', 4], // use 4 spaces
+        quotes: ['error', 'single'], // use single quote
+        'comma-dangle': [
+            // use , line end
+            'error',
+            {
+                arrays: 'always',
+                objects: 'always',
+                imports: 'always',
+                exports: 'always',
+                functions: 'never',
+            },
+        ],
+        'no-console': ['error', { allow: ['log', 'warn', 'error'] }], // allow log
+    },
+}
