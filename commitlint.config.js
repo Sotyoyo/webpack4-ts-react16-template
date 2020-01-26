889b6ec2b4818649335f3cd735ebea4e32b62663
Sotyoyo <sotyoyo@icloud.com>
2020-01-26 20:12:00 +0800

    build: with comitlint

---
+++ commitlint.config.js
@@ -0,0 +1,5 @@
+// https://www.conventionalcommits.org/zh-hans/v1.0.0-beta.4/
+// rule: type(scope?): subject # scope is optional
+// types: ['build', 'ci', 'chore', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'];
+
+module.exports = { extends: ['@commitlint/config-conventional'] };
